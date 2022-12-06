8f9c4ed97dbf0c5e33a8302d06d87e9c3dd9d50b
George Liu <liuxiwei@inspur.com>
2022-12-07 03:16:03 +0800

    Update the style of resetButton (#61)
    According to the localization requirements of IPS, update the
    background color and font color of resetButton.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>

--- src/views/ResourceManagement/Power/PowerIdleSaver.vue
+++ src/views/ResourceManagement/Power/PowerIdleSaver.vue
@@ -166,7 +166,7 @@
                 variant="secondary"
                 type="reset"
                 form="idle-power-saver"
-                class="ms-3"
+                class="ml-3 resetbtn"
               >
                 {{ $t('pagePower.idlePowerSubmitReset') }}
               </BButton>
