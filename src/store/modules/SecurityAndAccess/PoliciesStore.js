44098f82eb264a149fd7132c4e09b620d7642727
Renuka Sharanay Pundla <Renuka.Sharanya.Pundla@ibm.com>
2023-08-17 18:22:44 +0530

    reverted the changes for ssh BMC shell protocol

--- src/store/modules/SecurityAndAccess/PoliciesStore.js
+++ src/store/modules/SecurityAndAccess/PoliciesStore.js
@@ -222,7 +222,7 @@ const PoliciesStore = {
           );
         });
     },
-    async saveSshProtocolState({ commit, dispatch }, protocolEnabled) {
+    async saveSshProtocolState({ commit }, protocolEnabled) {
       commit('setSshProtocolEnabled', protocolEnabled);
       const ssh = {
         SSH: {
@@ -232,8 +232,6 @@ const PoliciesStore = {
       return await api
         .patch('/redfish/v1/Managers/bmc/NetworkProtocol', ssh)
         .then(() => {
-          // Getting protocol data here so that the ssh gets enabled/disabled
-          dispatch('getNetworkProtocolStatusAfterDelay');
           if (protocolEnabled) {
             return i18n.t('pagePolicies.toast.successEnableBmcShell');
           } else {
--- src/views/SecurityAndAccess/Policies/Policies.vue
+++ src/views/SecurityAndAccess/Policies/Policies.vue
@@ -395,10 +395,6 @@ export default {
       this.$store
         .dispatch('policies/saveSshProtocolState', state)
         .then((message) => {
-          this.startLoader();
-          setTimeout(() => {
-            this.endLoader();
-          }, 30000);
           this.successToast(message);
         })
         .catch(({ message }) => this.errorToast(message));
