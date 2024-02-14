4bcd624b2b3d154113d9bfda2aa486c1675f8f30
Nikhil Ashoka <a.nikhil@ibm.com>
2024-02-14 14:46:58 +0530

    1KW: Network settings implementation
    - Implement Network settings.
    - JIRA: https://jsw.ibm.com/browse/PFEBMC-1851
    
    Signed-off-by: Nikhil Ashoka <a.nikhil@ibm.com>

---
+++ src/views/Operations/ServerPowerOperations/NetworkSettingsModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-modal
+    id="modal-network-settings"
+    ref="modal"
+    size="xl"
+    :title="$t('pageServerPowerOperations.modal.networkSettings')"
+    title-tag="h2"
+    scrollable
+    centered
+    no-close-on-esc
+    no-close-on-backdrop
+  >
+    Network settings
+    <template #modal-footer>
+      <div class="w-100">
+        <b-button variant="light" size="sm">{{
+          $t('pageServerPowerOperations.modal.cancel')
+        }}</b-button>
+        <b-button variant="primary" size="sm">{{
+          $t('pageServerPowerOperations.modal.submit')
+        }}</b-button>
+      </div>
+    </template>
+  </b-modal>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  watch: {},
+  methods: {},
+};
+</script>
