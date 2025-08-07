7e5c2732563d35f3dd43fd190c6ff6ad0b11d0c8
Vedangi Mittal <vedangimittal3004@gmail.com>
2025-08-07 14:37:14 +0530

    Fixed dumps page modal and toast issue
    - Fixed modal issue - modal not closing on clicking 'Initiate dump'
    - Fixed toast issue - toast missing dump type
    - Defect: https://jazz07.rchland.ibm.com:13443/jazz/web/projects/CSSD#action=com.ibm.team.workitem.viewWorkItem&id=727825
    
    Signed-off-by: Vedangi Mittal <vedangimittal3004@gmail.com>

--- src/views/Logs/Dumps/DumpsPartitionModalConfirmation.vue
+++ src/views/Logs/Dumps/DumpsPartitionModalConfirmation.vue
@@ -44,6 +44,7 @@ import { ref, computed, nextTick } from 'vue';
 import StatusIcon from '@/components/Global/StatusIcon.vue';
 import useVuelidateComposable from '@/components/Composables/useVuelidateComposable';
 import { useVuelidate } from '@vuelidate/core';
+import eventBus from '@/eventBus';
 
 const emit = defineEmits(['ok']);
 
@@ -71,9 +72,8 @@ const rules = computed(() => ({
 const v$ = useVuelidate(rules, { confirmed });
 
 const closeModal = () => {
-      nextTick(() => {
-        modal.value=false;
-      });
+      modal.value=false;
+      eventBus.emit('partition-modal-close');
     };
 const handleSubmit = () => {
       v$.value.$touch();
